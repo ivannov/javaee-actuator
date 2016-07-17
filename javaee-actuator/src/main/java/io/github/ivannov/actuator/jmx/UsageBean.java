d463f3f99753e50483d82e730ae6c52453defd90
ivannov <ivan_st_ivanov@yahoo.com>
2016-07-17 21:46:15 +0300

    Added some more metrics

--- javaee-actuator/src/main/java/io/github/ivannov/actuator/jmx/UsageBean.java
+++ javaee-actuator/src/main/java/io/github/ivannov/actuator/jmx/UsageBean.java
@@ -10,6 +10,7 @@ import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Ivan St. Ivanov.
@@ -33,27 +34,69 @@ public class UsageBean {
         }
     }
 
-    public String getHeapMemoryUsageInMB() {
+    public int getProcessCPU() {
+        try {
+            double cpuUsage = (double) getAttributeValue("java.lang:type=OperatingSystem", "ProcessCpuLoad");
+            return (int) (cpuUsage * 100);
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    public Map<String, Long> getHeapMemory() {
+        Map<String, Long> heapMemory = new HashMap<>();
         try {
             CompositeData cd = (CompositeData) getAttributeValue("java.lang:type=Memory", "HeapMemoryUsage");
-            Long usedInMB = ((Long) cd.get("used")) / 1024 / 1024;
-            return usedInMB.toString() + " MB";
+            heapMemory.put("used", ((Long) cd.get("used")) / 1024);
+            heapMemory.put("committed", ((Long) cd.get("committed")) / 1024);
+            heapMemory.put("init", ((Long) cd.get("init")) / 1024);
+            heapMemory.put("max", ((Long) cd.get("max")) / 1024);
+            return heapMemory;
         } catch (Exception e) {
-            return "Holly smokes";
+            System.out.println(e.getMessage());
         }
+        return heapMemory;
+    }
+
+    public Map<String, Integer> getThreadsInfo() {
+        Map<String, Integer> threadsInfo = new HashMap<>();
+        try {
+            threadsInfo.put("threads", (Integer) getAttributeValue("java.lang:type=Threading", "ThreadCount"));
+            threadsInfo.put("threads.peak", (Integer) getAttributeValue("java.lang:type=Threading", "PeakThreadCount"));
+            threadsInfo.put("threads.daemon", (Integer) getAttributeValue("java.lang:type=Threading", "DaemonThreadCount"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return threadsInfo;
+    }
+
+    public Map<String, Long> getLoadedClassesInfo() {
+        Map<String, Long> loadedClassesInfo = new HashMap<>();
+        try {
+            loadedClassesInfo.put("classes", (Long) getAttributeValue("java.lang:type=ClassLoading", "TotalLoadedClassCount"));
+            loadedClassesInfo.put("classes.loaded", (long) (int) getAttributeValue("java.lang:type=ClassLoading", "LoadedClassCount"));
+            loadedClassesInfo.put("classes.unloaded", (Long) getAttributeValue("java.lang:type=ClassLoading", "UnloadedClassCount"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return loadedClassesInfo;
+    }
+
+    public Map<String, Long> getGCInfo() {
+        Map<String, Long> gcInfo = new HashMap<>();
+        try {
+            gcInfo.put("ps_scavenge.count", (Long) getAttributeValue("java.lang:type=GarbageCollector,name=PS Scavenge", "CollectionCount"));
+            gcInfo.put("ps_scavenge.time", (Long) getAttributeValue("java.lang:type=GarbageCollector,name=PS Scavenge", "CollectionTime"));
+            gcInfo.put("ps_marksweep.count", (Long) getAttributeValue("java.lang:type=GarbageCollector,name=PS MarkSweep", "CollectionCount"));
+            gcInfo.put("ps_marksweep.time", (Long) getAttributeValue("java.lang:type=GarbageCollector,name=PS MarkSweep", "CollectionTime"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return gcInfo;
     }
 
     private Object getAttributeValue(String objectName, String attributeName) throws Exception {
         return jmxConnector.getMBeanServerConnection().getAttribute(new ObjectName(objectName), attributeName);
-    }
-
-    public String getProcessCPU() {
-        try {
-            double cpuUsage = (double) getAttributeValue("java.lang:type=OperatingSystem", "ProcessCpuLoad");
-            return ((int) (cpuUsage * 100)) + " %";
-        } catch (Exception e) {
-            return "Holly smokes";
-        }
     }
 
     @PreDestroy
---
+++ javaee-actuator/src/main/java/io/github/ivannov/actuator/resources/MetricsResource.java
@@ -0,0 +1,37 @@
+package io.github.ivannov.actuator.resources;
+
+import io.github.ivannov.actuator.jmx.UsageBean;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import java.util.Map;
+
+@Path("/metrics")
+@RequestScoped
+public class MetricsResource {
+
+    @Inject
+    private UsageBean usageBean;
+
+	@GET
+	@Produces("application/json")
+	public Response getResourceUsage() {
+        JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder();
+        jsonObjectBuilder.add("cpu", usageBean.getProcessCPU());
+        Map<String, Long> heapMemory = usageBean.getHeapMemory();
+        heapMemory.entrySet().forEach(entry -> jsonObjectBuilder.add("heap." + entry.getKey(), entry.getValue()));
+        Map<String, Integer> threadInfo = usageBean.getThreadsInfo();
+        threadInfo.entrySet().forEach(entry -> jsonObjectBuilder.add(entry.getKey(), entry.getValue()));
+        Map<String, Long> loadedClassesInfo = usageBean.getLoadedClassesInfo();
+        loadedClassesInfo.entrySet().forEach(entry -> jsonObjectBuilder.add(entry.getKey(), entry.getValue()));
+        Map<String, Long> gcInfo = usageBean.getGCInfo();
+        gcInfo.entrySet().forEach(entry -> jsonObjectBuilder.add("gc." + entry.getKey(), entry.getValue()));
+        return Response.ok(jsonObjectBuilder.build()).build();
+	}
+}
