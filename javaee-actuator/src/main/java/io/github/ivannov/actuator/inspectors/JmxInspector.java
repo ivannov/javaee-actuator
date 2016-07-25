0fd0422a1be73f4aafa11217e8225e238d55214e
ivannov <ivan_st_ivanov@yahoo.com>
2016-07-25 23:20:18 +0300

    Use ManagementFactory API wherever possible

--- javaee-actuator/src/main/java/io/github/ivannov/actuator/inspectors/JmxInspector.java
+++ javaee-actuator/src/main/java/io/github/ivannov/actuator/inspectors/JmxInspector.java
@@ -3,12 +3,13 @@ package io.github.ivannov.actuator.inspectors;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
+import javax.management.MBeanServerConnection;
 import javax.management.ObjectName;
-import javax.management.openmbean.CompositeData;
 import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 import java.io.IOException;
+import java.lang.management.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,6 +22,7 @@ public class JmxInspector {
     private static final String JMX_HOST = "127.0.0.1";
 
     private JMXConnector jmxConnector;
+    private MBeanServerConnection remoteConnection;
 
     @PostConstruct
     public void initializeJMXConnector() {
@@ -29,6 +31,7 @@ public class JmxInspector {
             JMXServiceURL jmxServiceURL = new JMXServiceURL("rmi", "", 0, "/jndi/rmi://" + JMX_HOST + ":" + managementPort + "/jmxrmi");
             jmxConnector = JMXConnectorFactory.newJMXConnector(jmxServiceURL, new HashMap<>());
             jmxConnector.connect();
+            remoteConnection = jmxConnector.getMBeanServerConnection();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -36,6 +39,7 @@ public class JmxInspector {
 
     public int getProcessCPU() {
         try {
+            // No direct method in the operating system class to get the CPU load
             double cpuUsage = (double) getAttributeValue("java.lang:type=OperatingSystem", "ProcessCpuLoad");
             return (int) (cpuUsage * 100);
         } catch (Exception e) {
@@ -46,11 +50,15 @@ public class JmxInspector {
     public Map<String, Long> getHeapMemory() {
         Map<String, Long> heapMemory = new HashMap<>();
         try {
-            CompositeData cd = (CompositeData) getAttributeValue("java.lang:type=Memory", "HeapMemoryUsage");
-            heapMemory.put("used", ((Long) cd.get("used")) / 1024);
-            heapMemory.put("committed", ((Long) cd.get("committed")) / 1024);
-            heapMemory.put("init", ((Long) cd.get("init")) / 1024);
-            heapMemory.put("max", ((Long) cd.get("max")) / 1024);
+            MemoryMXBean memoryMXBean = ManagementFactory.newPlatformMXBeanProxy(
+                    remoteConnection,
+                    ManagementFactory.MEMORY_MXBEAN_NAME,
+                    MemoryMXBean.class);
+            MemoryUsage heapMemoryUsage = memoryMXBean.getHeapMemoryUsage();
+            heapMemory.put("used", heapMemoryUsage.getUsed() / 1024);
+            heapMemory.put("committed", heapMemoryUsage.getCommitted() / 1024);
+            heapMemory.put("init", heapMemoryUsage.getInit() / 1024);
+            heapMemory.put("max", heapMemoryUsage.getMax() / 1024);
             return heapMemory;
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -61,9 +69,13 @@ public class JmxInspector {
     public Map<String, Integer> getThreadsInfo() {
         Map<String, Integer> threadsInfo = new HashMap<>();
         try {
-            threadsInfo.put("threads", (Integer) getAttributeValue("java.lang:type=Threading", "ThreadCount"));
-            threadsInfo.put("threads.peak", (Integer) getAttributeValue("java.lang:type=Threading", "PeakThreadCount"));
-            threadsInfo.put("threads.daemon", (Integer) getAttributeValue("java.lang:type=Threading", "DaemonThreadCount"));
+            ThreadMXBean threadsMXBean = ManagementFactory.newPlatformMXBeanProxy(
+                    remoteConnection,
+                    ManagementFactory.THREAD_MXBEAN_NAME,
+                    ThreadMXBean.class);
+            threadsInfo.put("threads", threadsMXBean.getThreadCount());
+            threadsInfo.put("threads.peak", threadsMXBean.getPeakThreadCount());
+            threadsInfo.put("threads.daemon", threadsMXBean.getDaemonThreadCount());
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -73,9 +85,13 @@ public class JmxInspector {
     public Map<String, Long> getLoadedClassesInfo() {
         Map<String, Long> loadedClassesInfo = new HashMap<>();
         try {
-            loadedClassesInfo.put("classes", (Long) getAttributeValue("java.lang:type=ClassLoading", "TotalLoadedClassCount"));
-            loadedClassesInfo.put("classes.loaded", (long) (int) getAttributeValue("java.lang:type=ClassLoading", "LoadedClassCount"));
-            loadedClassesInfo.put("classes.unloaded", (Long) getAttributeValue("java.lang:type=ClassLoading", "UnloadedClassCount"));
+            ClassLoadingMXBean classLoadingMXBean = ManagementFactory.newPlatformMXBeanProxy(
+                    remoteConnection,
+                    ManagementFactory.CLASS_LOADING_MXBEAN_NAME,
+                    ClassLoadingMXBean.class);
+            loadedClassesInfo.put("classes", classLoadingMXBean.getTotalLoadedClassCount());
+            loadedClassesInfo.put("classes.loaded", (long) classLoadingMXBean.getLoadedClassCount());
+            loadedClassesInfo.put("classes.unloaded", classLoadingMXBean.getUnloadedClassCount());
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -85,6 +101,7 @@ public class JmxInspector {
     public Map<String, Long> getGCInfo() {
         Map<String, Long> gcInfo = new HashMap<>();
         try {
+            // In the Management Factory you find only one GC count and time. We need two (scavenge and marksweep)
             gcInfo.put("ps_scavenge.count", (Long) getAttributeValue("java.lang:type=GarbageCollector,name=PS Scavenge", "CollectionCount"));
             gcInfo.put("ps_scavenge.time", (Long) getAttributeValue("java.lang:type=GarbageCollector,name=PS Scavenge", "CollectionTime"));
             gcInfo.put("ps_marksweep.count", (Long) getAttributeValue("java.lang:type=GarbageCollector,name=PS MarkSweep", "CollectionCount"));
