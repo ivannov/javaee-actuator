482cb900c4a2eed789c75321d9e561ce15bb291a
Ivan St. Ivanov <ivan.ivanov@sap.com>
2016-08-24 10:45:13 +0300

    Added license information to the project files

--- javaee-actuator/src/main/java/io/github/ivannov/actuator/inspectors/BeansInspector.java
+++ javaee-actuator/src/main/java/io/github/ivannov/actuator/inspectors/BeansInspector.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.ivannov.actuator.inspectors;
 
 import javax.enterprise.context.RequestScoped;
@@ -8,9 +21,6 @@ import javax.enterprise.util.AnnotationLiteral;
 import javax.inject.Inject;
 import java.util.Set;
 
-/**
- * @author Ivan St. Ivanov.
- */
 @RequestScoped
 public class BeansInspector {
 
